65ad231ff4ce30d8109556e5ab68f564814b0621
vtHau <crtrunghau@gmail.com>
2021-11-19 21:34:26 +0700

    add productApi

--- src/services/ChatbotService.js
+++ src/services/ChatbotService.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import request from "request";
+import productApi from "../apis/productApi";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const IMAGE_STARTED =
