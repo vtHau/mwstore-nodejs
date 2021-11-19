c577fefd000169b93a0c87c4d4e5d1a5b9d8ae69
vtHau <crtrunghau@gmail.com>
2021-11-19 12:00:44 +0700

    add testdb ejs

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -7,7 +7,7 @@ const getHomePage = async (req, res) => {
 const testDB = async (req, res) => {
   try {
     const products = await db.products.findAll();
-    return res.render("homepage.ejs", {
+    return res.render("testdb.ejs", {
       products: JSON.stringify(products),
     });
   } catch (e) {
