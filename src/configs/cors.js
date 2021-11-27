fd6341e6c7bf639db3f72db8dfc7aebe43710a98
vtHau <crtrunghau@gmail.com>
2021-11-27 22:03:51 +0700

    add cors

--- src/configs/cors.js
+++ src/configs/cors.js
@@ -1,5 +1,6 @@
 const cors = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
--- src/server.js
+++ src/server.js
@@ -3,17 +3,20 @@ import express from "express";
 import http from "http";
 import viewEngine from "./configs/viewEngine";
 import webRoutes from "./routes/web";
-import cors from "./configs/cors";
+import cors from "cors";
 import ChatRealtime from "./services/ChatRealtime";
+const corsConfig = {
+  credentials: true,
+  origin: true,
+};
 
 const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server, { cors: { origin: "*" } });
-
+const io = require("socket.io")(server, { cors: corsConfig });
 const PORT = process.env.PORT || 9000;
 
 app.use(express.json());
-app.use(cors);
+app.use(cors(corsConfig));
 
 viewEngine(app);
 webRoutes(app);
